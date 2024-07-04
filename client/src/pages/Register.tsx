bb10d28bc6acebe346d893c49b5564ffb5949dfa
Musa Sayar <musasayar67@gmail.com>
2024-07-04 18:13:13 +0300

    edit register

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import RegisterSvg from "../assets/register.svg";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="max-w-xl mx-auto my-12">
+      <div className="flex items-center justify-center flex-col  mt-4">
+        <img src={RegisterSvg} alt="register"  className="rounded-full w-40 h-40  shadow-md "/>
+        <h1>Register</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
