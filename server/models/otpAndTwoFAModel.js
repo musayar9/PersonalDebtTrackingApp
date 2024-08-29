eb87a754a6ad168527a0bde9a6cd4e3f1ee0926d
Musa Sayar <musasayar67@gmail.com>
2024-08-29 13:11:57 +0300

    otpAntTwoFA model added

---
+++ server/models/otpAndTwoFAModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const otpAndTwoFAModel = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      index: true,
+      ref: "User",
+    },
+    verificationCode: {
+      type: String,
+      required: true,
+      index: true,
+      unique: true,
+      default: Math.floor(100000 + Math.random() * 900000).toString(),
+    },
+  },
+  { timestamps: true }
+);
+
+otpAndTwoFAModel.index({ createdAt: 1 }, { expiresAfterSeconds: 3600 });
+const otpAndTwoFA = mongoose.model("optAndTwoFA", otpAndTwoFAModel);
+
+module.exports = otpAndTwoFA;
