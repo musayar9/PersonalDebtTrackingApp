a623697af75f21e060fadf0cc82b52c7731a59ab
Musa Sayar <musasayar67@gmail.com>
2024-09-13 16:35:08 +0300

    edit checkPasswordCriteria component

--- client/src/components/CheckPasswordCriteria.tsx
+++ client/src/components/CheckPasswordCriteria.tsx
@@ -7,27 +7,27 @@ const CheckPasswordCriteria = ({password}:{password:string}) => {
   return (
     <div>
       {" "}
-      <ul className="list-disc grid grid-cols-4  font-semibold  text-[10px] pl-4 g">
+      <ul className="list-disc grid grid-cols-3  font-semibold  text-[10px] pl-4 g">
         <li
           className={`${
             criteria.hasLowerCase ? "text-green-500" : "text-slate-400"
           }`}
         >
-          En az bir küçük harf
+          At least one lowercase letter
         </li>
         <li
           className={`${
             criteria.hasUpperCase ? "text-green-500" : "text-slate-400"
           }`}
         >
-          En az bir büyük harf
+          At least one uppercase letter
         </li>
         <li
           className={`${
             criteria.hasDigit ? "text-green-500" : "text-slate-400"
           }`}
         >
-          En az bir rakam
+          At least one digit
         </li>
       </ul>
       <ul className="list-disc grid grid-cols-2 font-semibold  text-[10px] pl-4 ">
@@ -36,14 +36,14 @@ const CheckPasswordCriteria = ({password}:{password:string}) => {
             criteria.hasSpecialChar ? "text-green-500" : "text-slate-400"
           }`}
         >
-          En az bir özel karakter (@$!%*?&)
+          At least one special character (@$!%*?&.)
         </li>
         <li
           className={`${
             criteria.hasMinLength ? "text-green-500" : "text-slate-400"
           }`}
         >
-          Şifre 8 ile 12 karakter uzunluğunda olmalı.
+          The password must be 8 to 12 characters long
         </li>
       </ul>
     </div>
