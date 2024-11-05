27b3fc8929cfbf36e7a2337ba1c4ec3c939b4e47
Musa Sayar <musasayar67@gmail.com>
2024-11-06 00:26:30 +0300

    edit react-dom declaration

--- client/src/declarations.d.ts
+++ client/src/declarations.d.ts
@@ -7,3 +7,18 @@ declare module "*.png" {
   const content: string;
   export default content;
 }
+
+
+
+declare module 'react-dom/client' {
+  import { ReactNode } from 'react';
+  import { Root } from 'react-dom';
+
+  interface Root {
+    render(children: ReactNode): void;
+    unmount(): void;
+  }
+
+  function createRoot(container: Element | DocumentFragment): Root;
+  export { createRoot };
+}
