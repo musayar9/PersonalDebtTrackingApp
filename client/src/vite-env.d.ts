037ed35d25e71d47f745e628138fe52011c29cdb
Musa Sayar <musasayar67@gmail.com>
2024-11-06 00:34:57 +0300

    edit vite-env.d.ts

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_APPID: string;
+  // Diğer ortam değişkenlerini buraya ekleyin
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
